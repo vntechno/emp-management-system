732df58137382f90e1347f5cc667187c352c492f
vntechno <singhvikash.info@gmail.com>
2023-02-03 22:23:48 +0530

    open API docs added

--- src/main/java/com/vrtech/ems/EmpManagementSystemApplication.java
+++ src/main/java/com/vrtech/ems/EmpManagementSystemApplication.java
@@ -3,7 +3,15 @@ package com.vrtech.ems;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info= @Info(
+        title = "Employee management System API",
+        version = "1.0"
+    ))
+
 public class EmpManagementSystemApplication {
 
 	public static void main(String[] args) {
